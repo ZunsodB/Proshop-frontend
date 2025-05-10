68d030dc68979f5194879f3f92424d36cb7aa88c
ZunsodB <s21c118b@nmct.edu.mn>
2025-05-10 20:56:25 +0800

    a

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,5 @@
+import dotenv from 'dotenv';
+dotenv.config();
 // export const BASE_URL =
 //   process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5000' : '';
 export const BASE_URL = process.env.BACKEND_URL; // If using proxy
